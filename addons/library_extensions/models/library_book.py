c4e2fd137e45bd1183da504f411b4071d8eecd08
hygenekit <lopezgenelouise@gmail.com>
2025-08-21 14:51:35 +0800

    Update library_book.py

--- addons/library_extensions/models/library_book.py
+++ addons/library_extensions/models/library_book.py
@@ -1,18 +1,29 @@
-from odoo import fields, models
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class LibraryBook(models.Model):
-    _inherit = "library.book"
+    _inherit = 'library.book'
 
     author_id = fields.Many2one(
-        "res.partner",
-        string="Author",
-        required=True,
+        'res.partner',
+        string='Author',
+        required=True
     )
 
     category_id = fields.Many2many(
-        "library.book.category",
-        "library_book_category_rel",
-        "book_id",
-        "category_id",
-        string="Categories"
+        'library.book.category',
+        string='Categories'
     )
+
+
+class LibraryBookCategory(models.Model):
+    _name = 'library.book.category'
+    _description = 'Book Category'
+
+    name = fields.Char(string='Category Name', required=True, unique=True)
+
+    _sql_constraints = [
+        ('name_unique',
+         'unique(name)',
+         'Category name must be unique!')
+    ]
